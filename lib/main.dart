0e5c64f6a9571ac3f602536b61abc81e6cb68816
Achut_Prasad_Wagle <achutw35@gmail.com>
2026-02-04 13:28:44 +0000

    Get method use in main app as GetMaterialApp using package:get/get.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:helloworld/view/home_view.dart';
 
 void main() {
@@ -8,7 +9,7 @@ void main() {
 class ToDoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'My App',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: HomeView(),
